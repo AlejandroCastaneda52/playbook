efceb2f40eb464018c591e8800f364b07eb20723
AlejandroCastaneda52 <99110317+AlejandroCastaneda52@users.noreply.github.com>
2022-04-29 19:30:43 -0500

    Add files via upload

---
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -0,0 +1,15 @@
+class Spiderman{
+    constructor(name,age,actor,movies,studio){
+        this.name = name
+        this.age = age
+        this.actor = actor
+        this.movies = movies
+        this.studio = studio
+        
+    }
+    getInfo(){
+        return "Hey, I'm Tom Holland from Marvel studio"
+    }
+}
+
+module.exports = Spiderman
